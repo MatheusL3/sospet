39c5a5c57a8fc79e71fdaf6f2f247e225e6559f3
Matheus L3 <m.sosinformatica@gmail.com>
2020-09-30 11:52:24 -0300

    Fim do curso crud simples com Django

--- core/views.py
+++ core/views.py
@@ -6,6 +6,42 @@ from django.views.decorators.csrf import csrf_protect
 from .models import Pet
 
 
+@login_required(login_url='/login/')
+def set_pet(request):
+    city = request.POST.get('city')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    description = request.POST.get('description')
+    file = request.FILES.get('file')
+    user = request.user
+    pet_id = request.POST.get('pet_id')
+    if pet_id:
+        pet = Pet.objects.get(id=pet_id)
+        if user == pet.user:
+            pet.email = email
+            pet.phone = phone
+            pet.city = city
+            pet.description = description
+            if file:
+                pet.photo = file
+            pet.save()
+    else:
+        pet = Pet.objects.create(email=email, phone=phone, city=city, description=description,
+                                user=user, photo=file)
+    url = '/pet/detail/{}/'.format(pet.id)
+    return redirect(url)
+
+
+@login_required(login_url='/login/')
+def register_pet(request):
+    pet_id = request.GET.get('id')
+    if pet_id:
+        pet = Pet.objects.get(id=pet_id)
+        if pet.user == request.user:
+            return render(request, 'register.html', {'pet':pet})
+    return render(request, 'register.html')
+
+
 def login_user(request):
     return render(request,'login.html')
 
@@ -29,7 +65,35 @@ def logout_user(request):
     return redirect('/login/')
 
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def list_all_pets(request):
     pet = Pet.objects.filter(active=True)
-    return render(request, 'list.html',{'pet':pet})
+    return render(request, 'list.html', {'pet': pet})
+
+
+@login_required(login_url='/login/')
+def list_user_pets(request):
+    pet = Pet.objects.filter(active=True, user=request.user)
+    return render(request, 'list.html', {'pet': pet})
+
+
+@login_required(login_url='/login/')
+def pet_detail(request, id):
+    pet = Pet.objects.get(active=True, id=id)
+    print(pet.description)
+    return render(request, 'detail.html',  {'pet': pet})
+
+
+@login_required(login_url='/login/')
+def pet_delete(request, id):
+    pet = Pet.objects.get(active=True, id=id)
+    if pet and pet.user == request.user:
+        status = Pet.delete(pet)
+        if status:
+            print('cachorro excluido com sucesso')
+        else:
+            print('NAO FOI POSSÍVEL EXCLUIR')
+    else:
+        print('REGISTRO INEXISTENTE OU ESSE CACHORRO NÃO É SEU')
+    return redirect('/')
+
--- sospet/urls.py
+++ sospet/urls.py
@@ -24,10 +24,15 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_user),
-    path('list/all/', views.list_all_pets),
-    path('login/submit', views.submit_login),
+    path('pet/all/', views.list_all_pets),
+    path('pet/register/', views.register_pet),
+    path('pet/register/submit/', views.set_pet),
+    path('pet/detail/<id>/', views.pet_detail),
+    path('pet/delete/<id>/', views.pet_delete),
+    path('pet/user/', views.list_user_pets),
+    path('login/submit/', views.submit_login),
     path('logout/', views.logout_user),
-    path('', RedirectView.as_view(url='list/all/'))
+    path('', RedirectView.as_view(url='pet/all/'))
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
