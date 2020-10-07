ac7f220cf2045dd1716a9f661ec12dfb76630451
Matheus L3 <m.sosinformatica@gmail.com>
2020-10-06 21:02:50 -0300

    atualização models.py places

--- sospet/places/models.py
+++ sospet/places/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class Places(models.Model):
-    title = models.CharField( max_length=100),
-    slug = models.SlugField(),
-    description = models.TextField(blank=True),
-    created_at = models.DateTimeField(auto_now_add=True),
-    updated_at = models.DateTimeField(auto_now=True),
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='places', verbose_name='Imagens')
 
     def __str__(self):
